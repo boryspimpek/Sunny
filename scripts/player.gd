72c206cf482a5a03e8d8086f28677d18b3e1c86d
boryspimpek <borysgame@gmail.com>
2026-07-20 16:45:08 +0200

    aim

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,6 +11,9 @@ const PROJECTILE_SCENE: PackedScene = preload("res://scenes/projectile.tscn")
 @export var gamepad_camera_sensitivity: float = 2.5
 @export var roll_distance: float = 5.0
 @export var fire_interval: float = 0.2
+@export var aim_assist_strength: float = 4.0
+@export_range(1.0, 45.0, 1.0, "degrees") var aim_assist_cone: float = 18.0
+@export var aim_assist_range: float = 20.0
 @export_range(-89.0, 0.0, 1.0, "degrees") var camera_pitch_min: float = -45.0
 @export_range(-30.0, 89.0, 1.0, "degrees") var camera_pitch_max: float = 45.0
 
@@ -24,6 +27,7 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var model: Node3D = $Skeleton3D
 @onready var spring_arm_pivot: Node3D = $SpringArmPivot
 @onready var spring_arm: SpringArm3D = $SpringArmPivot/SpringArm3D
+@onready var camera: Camera3D = $SpringArmPivot/SpringArm3D/Camera3D
 
 var action_animation_playing := false
 var roll_hips_bone := -1
@@ -69,9 +73,38 @@ func _on_animation_finished(animation_name: StringName) -> void:
 		action_animation_playing = false
 		animation_tree.active = true
 
+func _apply_aim_assist(delta: float) -> void:
+	var camera_forward := -camera.global_transform.basis.z
+	camera_forward.y = 0.0
+	camera_forward = camera_forward.normalized()
+	var target: Node3D
+	var best_alignment := cos(deg_to_rad(aim_assist_cone))
+
+	for node in get_tree().get_nodes_in_group("enemy"):
+		var enemy := node as Node3D
+		if enemy == null:
+			continue
+		var direction_to_enemy := enemy.global_position - camera.global_position
+		direction_to_enemy.y = 0.0
+		if direction_to_enemy.length() > aim_assist_range or direction_to_enemy.is_zero_approx():
+			continue
+		var alignment := camera_forward.dot(direction_to_enemy.normalized())
+		if alignment > best_alignment:
+			best_alignment = alignment
+			target = enemy
+
+	if target != null:
+		var target_direction := target.global_position - camera.global_position
+		target_direction.y = 0.0
+		var angle_to_target := camera_forward.signed_angle_to(target_direction.normalized(), Vector3.UP)
+		var max_rotation := aim_assist_strength * delta
+		spring_arm_pivot.rotate_y(clampf(angle_to_target, -max_rotation, max_rotation))
+
 func _shoot() -> void:
 	var projectile: Projectile = PROJECTILE_SCENE.instantiate()
-	var direction := model.global_transform.basis.z.normalized()
+	var direction := -camera.global_transform.basis.z
+	direction.y = 0.0
+	direction = direction.normalized()
 	get_parent().add_child(projectile)
 	projectile.global_position = global_position + direction + Vector3.UP
 	projectile.setup(direction)
@@ -95,8 +128,10 @@ func _physics_process(delta: float) -> void:
 		velocity.y -= gravity * delta
 
 	fire_cooldown = maxf(fire_cooldown - delta, 0.0)
-	if Input.is_action_pressed("fire") and fire_cooldown <= 0.0:
-		_shoot()
+	if Input.is_action_pressed("fire"):
+		_apply_aim_assist(delta)
+		if fire_cooldown <= 0.0:
+			_shoot()
 
 	# Skok
 	if Input.is_action_just_pressed("jump") and is_on_floor() and not action_animation_playing:
