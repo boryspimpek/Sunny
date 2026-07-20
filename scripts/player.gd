7f106ce06c724f7995bc0dba28ca6cfbac545e94
boryspimpek <borysgame@gmail.com>
2026-07-20 13:57:16 +0200

    roll

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,6 +7,7 @@ extends CharacterBody3D
 @export var jump_strength: float = 8.0  # Siła skoku
 @export var rotation_speed: float = 12.0 # Szybkość obracania postaci
 @export var gamepad_camera_sensitivity: float = 2.5
+@export var roll_distance: float = 5.0
 @export_range(-89.0, 0.0, 1.0, "degrees") var camera_pitch_min: float = -45.0
 @export_range(-30.0, 89.0, 1.0, "degrees") var camera_pitch_max: float = 45.0
 
@@ -16,14 +17,61 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 # --- Referencje do węzłów ---
 # @onready pobiera węzeł AnimationTree zaraz po uruchomieniu gry
 @onready var animation_tree: AnimationTree = $AnimationTree
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var model: Node3D = $Skeleton3D
 @onready var spring_arm_pivot: Node3D = $SpringArmPivot
 @onready var spring_arm: SpringArm3D = $SpringArmPivot/SpringArm3D
 
+var action_animation_playing := false
+var roll_hips_bone := -1
+var roll_root_motion_start := Vector3.ZERO
+var roll_direction := Vector3.ZERO
+var roll_displacement_pending := Vector3.ZERO
+var roll_pivot_initial_position := Vector3.ZERO
+
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	animation_tree.active = true
+	roll_hips_bone = model.find_bone("mixamorig_Hips")
+	animation_player.animation_finished.connect(_on_animation_finished)
+
+func _play_action_animation(animation_name: StringName) -> void:
+	if action_animation_playing:
+		return
+
+	action_animation_playing = true
+	animation_tree.active = false
+	animation_player.play(animation_name)
+	if animation_name == &"Moves/roll" and roll_hips_bone != -1:
+		animation_player.advance(0.0)
+		roll_direction = model.global_transform.basis.z.normalized()
+		roll_pivot_initial_position = spring_arm_pivot.position
+		roll_root_motion_start = model.get_bone_global_pose(roll_hips_bone).origin
+
+func _update_roll_camera_position() -> void:
+	if roll_hips_bone == -1:
+		return
+
+	var current_root_motion: Vector3 = model.get_bone_global_pose(roll_hips_bone).origin
+	var root_motion_offset: Vector3 = current_root_motion - roll_root_motion_start
+	root_motion_offset.y = 0.0
+	spring_arm_pivot.position = roll_pivot_initial_position + model.transform.basis * root_motion_offset
+
+func _on_animation_finished(animation_name: StringName) -> void:
+	if animation_name == &"Moves/jump" or animation_name == &"Moves/roll":
+		if animation_name == &"Moves/roll":
+			roll_displacement_pending = roll_direction * roll_distance
+		action_animation_playing = false
+		animation_tree.active = true
 
 func _physics_process(delta: float) -> void:
+	if action_animation_playing and animation_player.current_animation == &"Moves/roll":
+		_update_roll_camera_position()
+	if not roll_displacement_pending.is_zero_approx():
+		move_and_collide(roll_displacement_pending)
+		spring_arm_pivot.position = roll_pivot_initial_position
+		roll_displacement_pending = Vector3.ZERO
+
 	var camera_input := Input.get_vector("camera_left", "camera_right", "camera_up", "camera_down")
 	spring_arm_pivot.rotate_y(-camera_input.x * gamepad_camera_sensitivity * delta)
 	spring_arm.rotate_x(-camera_input.y * gamepad_camera_sensitivity * delta)
@@ -34,8 +82,11 @@ func _physics_process(delta: float) -> void:
 		velocity.y -= gravity * delta
 
 	# Skok
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor() and not action_animation_playing:
 		velocity.y = jump_strength
+		_play_action_animation(&"Moves/jump")
+	if Input.is_action_just_pressed("roll") and is_on_floor():
+		_play_action_animation(&"Moves/roll")
 
 	# 2. Odczytanie wejścia od gracza (WASD)
 	var input_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
@@ -58,5 +109,6 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 	# 6. Aktualizacja AnimationTree (mieszanie animacji idle/running)
-	var blend_value := Vector2(input_dir.x, -input_dir.y)
-	animation_tree.set("parameters/blend_position", blend_value)
+	if not action_animation_playing:
+		var blend_value := Vector2(input_dir.x, -input_dir.y)
+		animation_tree.set("parameters/blend_position", blend_value)
