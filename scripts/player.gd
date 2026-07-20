0045304bd3b99cb32fcaf72a0786da1016959a84
boryspimpek <borysgame@gmail.com>
2026-07-20 14:33:31 +0200

    Convert controller to isometric twin-stick

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,10 +6,7 @@ extends CharacterBody3D
 @export var friction: float = 8.0       # Jak szybko postać się zatrzymuje
 @export var jump_strength: float = 8.0  # Siła skoku
 @export var rotation_speed: float = 12.0 # Szybkość obracania postaci
-@export var gamepad_camera_sensitivity: float = 2.5
 @export var roll_distance: float = 5.0
-@export_range(-89.0, 0.0, 1.0, "degrees") var camera_pitch_min: float = -45.0
-@export_range(-30.0, 89.0, 1.0, "degrees") var camera_pitch_max: float = 45.0
 
 # Grawitacja pobrana z ustawień projektu Godota
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -73,11 +70,6 @@ func _physics_process(delta: float) -> void:
 		spring_arm_pivot.position = roll_pivot_initial_position
 		roll_displacement_pending = Vector3.ZERO
 
-	var camera_input := Input.get_vector("camera_left", "camera_right", "camera_up", "camera_down")
-	spring_arm_pivot.rotate_y(-camera_input.x * gamepad_camera_sensitivity * delta)
-	spring_arm.rotate_x(-camera_input.y * gamepad_camera_sensitivity * delta)
-	spring_arm.rotation.x = clamp(spring_arm.rotation.x, deg_to_rad(camera_pitch_min), deg_to_rad(camera_pitch_max))
-
 	# 1. Obsługa grawitacji (spadanie na ziemię)
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -89,19 +81,23 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("roll") and is_on_floor():
 		_play_action_animation(&"Moves/roll")
 
-	# 2. Odczytanie wejścia od gracza (WASD)
-	var input_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	# 2. Lewy drążek: ruch względem nieruchomej kamery
+	var move_input := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var move_direction := Vector3(move_input.x, 0.0, move_input.y)
+	move_direction = move_direction.rotated(Vector3.UP, spring_arm_pivot.global_rotation.y)
 
-	# 3. Kierunek ruchu względem kamery (spłaszczony na płaszczyźnie XZ)
-	var target_direction := Vector3(input_dir.x, 0.0, input_dir.y)
-	target_direction = target_direction.rotated(Vector3.UP, spring_arm_pivot.global_rotation.y)
-
-	# 4. Obrót postaci w stronę ruchu i płynne przyspieszanie/hamowanie
-	if target_direction.length() > 0.01:
-		var target_angle: float = spring_arm_pivot.global_rotation.y + PI - global_rotation.y
+	# 3. Prawy drążek: niezależny kierunek celowania
+	var aim_input := Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
+	var aim_direction := Vector3(aim_input.x, 0.0, aim_input.y)
+	aim_direction = aim_direction.rotated(Vector3.UP, spring_arm_pivot.global_rotation.y)
+	if aim_direction.length() > 0.01:
+		var target_angle := atan2(aim_direction.x, aim_direction.z)
 		model.rotation.y = lerp_angle(model.rotation.y, target_angle, rotation_speed * delta)
-		velocity.x = move_toward(velocity.x, target_direction.x * max_speed, acceleration * delta)
-		velocity.z = move_toward(velocity.z, target_direction.z * max_speed, acceleration * delta)
+
+	# 4. Ruch postaci
+	if move_direction.length() > 0.01:
+		velocity.x = move_toward(velocity.x, move_direction.x * max_speed, acceleration * delta)
+		velocity.z = move_toward(velocity.z, move_direction.z * max_speed, acceleration * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0.0, friction * delta)
 		velocity.z = move_toward(velocity.z, 0.0, friction * delta)
@@ -111,5 +107,5 @@ func _physics_process(delta: float) -> void:
 
 	# 6. Aktualizacja AnimationTree (mieszanie animacji idle/running)
 	if not action_animation_playing:
-		var blend_value := Vector2(input_dir.x, -input_dir.y)
+		var blend_value := Vector2(move_input.x, -move_input.y)
 		animation_tree.set("parameters/blend_position", blend_value)
