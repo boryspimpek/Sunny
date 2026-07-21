4b89c57ddebb80c499bbfec43a859665f22f8894
boryspimpek <borysgame@gmail.com>
2026-07-21 11:04:24 +0200

    map colision

---
+++ assets/maps/inferno/inferno.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+
+@export var generate_collision: bool = true
+
+
+func _ready() -> void:
+	if not generate_collision:
+		return
+
+	for mesh_instance: MeshInstance3D in find_children("*", "MeshInstance3D", true):
+		var shape: ConcavePolygonShape3D = mesh_instance.mesh.create_trimesh_shape()
+		if shape == null or shape.get_faces().size() == 0:
+			continue
+
+		var body := StaticBody3D.new()
+		var collision := CollisionShape3D.new()
+		body.name = mesh_instance.name + "_StaticBody"
+		collision.shape = shape
+		body.add_child(collision)
+		mesh_instance.add_sibling(body)
+		body.global_transform = mesh_instance.global_transform
