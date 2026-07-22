80616b68caa22a71ecae252c8b4f4de410231009
boryspimpek <borysgame@gmail.com>
2026-07-22 19:26:38 +0200

    arena

--- assets/maps/inferno/inferno.gd
+++ assets/maps/inferno/inferno.gd
@@ -9,6 +9,9 @@ func _ready() -> void:
 		return
 
 	for mesh_instance: MeshInstance3D in find_children("*", "MeshInstance3D", true):
+		if mesh_instance.mesh == null:
+			print("MeshInstance3D with null mesh: ", mesh_instance.name, " (", mesh_instance.get_path(), ")")
+			continue
 		var shape: ConcavePolygonShape3D = mesh_instance.mesh.create_trimesh_shape()
 		if shape == null or shape.get_faces().size() == 0:
 			continue
