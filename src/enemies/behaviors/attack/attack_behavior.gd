97304747b7d1526cd6990d11c28126afd4344b05
boryspimpek <borysgame@gmail.com>
2026-07-26 09:51:38 +0200

    zombie attack

--- src/enemies/behaviors/attack/attack_behavior.gd
+++ src/enemies/behaviors/attack/attack_behavior.gd
@@ -4,5 +4,6 @@ extends Node
 ## sceny wroga - Enemy wykryje go automatycznie. Baza = brak ataku.
 
 
+## Wywoływane przez Enemy w każdej klatce _physics_process.
 func try_attack(_enemy: Enemy, _target: Node3D, _delta: float) -> void:
 	pass
---
+++ src/enemies/behaviors/attack/zombnie_punch.gd
@@ -0,0 +1,32 @@
+class_name ZombiePunch
+extends AttackBehavior
+## Wróg atakuje wręcz, gdy gracz znajdzie się w zasięgu.
+
+@export var attack_range: float = 1.5
+@export var attack_cooldown: float = 1.0
+@export var attack_animation: StringName = "ZombieMoves/punching"
+
+var _cooldown: float = 0.0
+
+
+func try_attack(enemy: Enemy, target: Node3D, delta: float) -> void:
+	if target == null:
+		return
+
+	if _cooldown > 0.0:
+		_cooldown -= delta
+		return
+
+	var distance := enemy.global_position.distance_to(target.global_position)
+	if distance > attack_range:
+		return
+
+	var anim := enemy.get_node_or_null("AnimationPlayer") as AnimationPlayer
+	if anim == null:
+		return
+
+	if anim.current_animation == attack_animation and anim.is_playing():
+		return
+
+	anim.play(attack_animation)
+	_cooldown = attack_cooldown
